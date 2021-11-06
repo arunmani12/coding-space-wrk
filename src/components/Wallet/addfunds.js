edeca2fce9fe1579de368ef96a3614adc808eacf
arunmani <marun1156@gmail.com>
2021-11-06 16:32:26 +0530

    fund added

---
+++ src/components/Wallet/addfunds.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Money from './images/money-bag.png';
+import './addfunds.css'
+import UpBar from "./UpBar";
+
+const Addfunds = () =>{
+    return(
+        <>
+        
+        <UpBar/>
+
+        <div className="container1">
+            <div className="first_contain1">
+            <div className="sub_first_contain1">
+                <div className="title_contain1">
+                    <h3>Add Funds to Your Activation Wallet</h3>
+                    <br/>
+                    <form>
+                    <label id="lable1" for="addfund">Enter Amount in (TRX) </label>
+                    <input type="number" id="addfund" name="AddFunds" />
+                    <br/><br/>
+                        <button id="submitbtn">Submit</button>
+                    </form>
+                    <br/>
+                    <p className="title_contain1">
+                    Note: Once you press Submit, The system will show you the Tron Address to Transfer and the Required Amount of Trons to Transfer. Once you transfer,
+                    your wallet will be loaded automatically after the required number of confirmations. It may take some time for confirmations.
+                     Please send the exact amount of Coins shown in the next page
+                    </p>
+                </div>
+               
+
+            </div>
+            
+
+            </div>
+        </div>
+        
+</>
+    
+    )};
+export default Addfunds;

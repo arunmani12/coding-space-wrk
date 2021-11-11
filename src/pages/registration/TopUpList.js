1e56b04c6b18352c663d829bb2a3b29289a6906b
arunmani <marun1156@gmail.com>
2021-11-11 23:31:12 +0530

    added topup fetching

--- src/pages/registration/TopUpList.js
+++ src/pages/registration/TopUpList.js
@@ -1,24 +1,51 @@
 import React from "react";
-const [data, setData] = useState([])
-
+import {useEffect,useState} from 'react'
+// import TestData from './TestTopUp.json' -- for testing purpose
 
 const TopUpList = () => {
+
+
+  // const [data, setData] = useState(TestData) -- for testing purpose
+  const [data, setData] = useState([])
+
+  const [loading, setloading] = useState(false) 
+
+
+  useEffect(() => {
+    setloading(true)
+    let fetchData = async ()=>{
+      try{
+        let res = await fetch('http://FreeCoinApi.com/user/topupReport/618d2d5bb58f7202eaf4c569')
+        let data = await res.json()
+        setData(data)
+        setloading(false)
+      }
+      catch(e){
+        console.log(e)
+        setloading(false)
+      }
+    }
+   fetchData()
+  },[])
+
+
   return (
     <div className="card-container">
+      {loading&&<p>Loading...</p>}
       <h4 className="page-title">Topup Details and Registrations</h4>
       <table>
         <tr>
-          <th>TopUp User Name</th>
-          <th>TopUp Date</th>
+          <th>Type</th>
+          <th>transactionAt</th>
           <th>Amount</th>
-          <th>Status</th>
+          <th>message</th>
         </tr>
-        <tr>
-          <td>1</td>
-          <td>19/09/2021</td>
-          <td>TRX 200.00</td>
-          <td>707123</td>
-        </tr>
+        {data.map((d,i)=><tr key={i}>
+          <td>{d.type}</td>
+          <td>{d.transactionAt}</td>
+          <td>{d.amount}</td>
+          <td>{d.message}</td>
+        </tr>)}
       </table>
     </div>
   );
--- src/pages/withdraw/WithdrawReport.js
+++ src/pages/withdraw/WithdrawReport.js
@@ -1,26 +1,53 @@
 import React from "react";
-
+import {useEffect,useState} from 'react'
 import "./WithdrawReport.css";
+// import withDrawTest from './TestWithDraw.json' -- for testing purpose
 
 const WithdrawReport = () => {
+
+  // const [data, setData] = useState(withDrawTest) -- for testing purpose
+
+  const [data, setData] = useState([])
+
+  const [loading, setloading] = useState(false) 
+
+
+  useEffect(() => {
+    setloading(true)
+    let fetchData = async ()=>{
+      try{
+        let res = await fetch('http://FreeCoinApi.com/user/incomeReport/618d2d5bb58f7202eaf4c569')
+        let data = await res.json()
+        setData(data)
+        setloading(false)
+      }
+      catch(e){
+        console.log(e)
+        setloading(false)
+      }
+    }
+   fetchData()
+  },[])
+
   return (
     <div className="card-container">
-      <h4 className="page-title">Withdraw Report of 707123</h4>
-      <table>
-        <tr>
-          <th>Serial</th>
-          <th>Date</th>
-          <th>Amount</th>
-          <th>Trans ID</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>19/09/2021</td>
-          <td>TRX 200.00</td>
-          <td>707123</td>
-        </tr>
-      </table>
-    </div>
+    {loading&&<p>Loading...</p>}
+    <h4 className="page-title">Topup Details and Registrations</h4>
+    <table>
+      <tr>
+        <th>Type</th>
+        <th>transactionAt</th>
+        <th>Amount</th>
+        <th>message</th>
+      </tr>
+      {data.map((d,i)=><tr key={i}>
+        <td>{d.type}</td>
+        <td>{d.transactionAt}</td>
+        <td>{d.amount}</td>
+        <td>{d.message}</td>
+      </tr>)}
+    </table>
+  </div>
   );
 };
 
